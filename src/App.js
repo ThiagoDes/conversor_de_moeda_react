dfe030aeb808bff286a685400a30133ab28ba12e
ThiagoDes008 <thiago.gomes@fusiondms.com.br>
2020-12-23 17:03:12 -0300

    update 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Coversor from "./Componentes/Conversor"
+
+function App() {
+  return (
+    <div className="App">
+        <Coversor moedaA="USD" moedaB="BRL">
+
+        </Coversor>
+    </div>
+  );
+}
+
+export default App;
